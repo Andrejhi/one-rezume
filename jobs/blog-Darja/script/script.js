bb540228927d3224c476f53e634c13aad1664554
andru <room861@yandex.ru>
2020-01-04 17:34:56 +0300

    Add files via upload

--- jobs/blog-Darja/script/script.js
+++ jobs/blog-Darja/script/script.js
@@ -1,6 +1,22 @@
 const searchForm = document.querySelector('.search_form'),
-    searchHeader = document.querySelector('.search_header');
+    searchHeader = document.querySelector('.search_header'),
+    searchIcon = document.querySelector('.search_icon'),
+    closeSearch = document.querySelector('.close_search'),
+    openSearch = document.querySelector('.open-search');
 
-searchHeader.addEventListener('click', () => {
+searchIcon.addEventListener('click', () => {
     searchForm.classList.add('open_search');
+    searchIcon.style.display = 'none';
+    closeSearch.style.display = 'flex';
+});
+
+closeSearch.addEventListener('click', () => {
+    searchForm.style.top = '-150%';
+    searchIcon.style.display = 'flex';
+    closeSearch.style.display = 'none';
+});
+searchIcon.addEventListener('click', () => {
+    searchForm.style.top = '96px';
+    searchIcon.style.display = 'none';
+    closeSearch.style.display = 'flex';
 });
