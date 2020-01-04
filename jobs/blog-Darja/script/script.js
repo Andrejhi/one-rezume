ba6b4304a1f5b33a05baa0f6aa0e3c8edd5a0c25
andru <room861@yandex.ru>
2020-01-04 22:52:52 +0300

    Add files via upload

--- jobs/blog-Darja/script/script.js
+++ jobs/blog-Darja/script/script.js
@@ -2,7 +2,8 @@ const searchForm = document.querySelector('.search_form'),
     searchHeader = document.querySelector('.search_header'),
     searchIcon = document.querySelector('.search_icon'),
     closeSearch = document.querySelector('.close_search'),
-    openSearch = document.querySelector('.open-search');
+    openSearch = document.querySelector('.open-search'),
+    searchFormSmart = document.querySelector('.search_form_smart');
 
 searchIcon.addEventListener('click', () => {
     searchForm.classList.add('open_search');
@@ -17,6 +18,35 @@ closeSearch.addEventListener('click', () => {
 });
 searchIcon.addEventListener('click', () => {
     searchForm.style.top = '81px';
+    searchFormSmart.style.top = '57px';
     searchIcon.style.display = 'none';
     closeSearch.style.display = 'flex';
 });
+
+const images = document.querySelectorAll('img');
+
+const options = {
+    root : null,
+    rootMargin: '0px',
+    threshold : 0.1
+}
+
+function handleImg( myImg, observer){
+    myImg.forEach(myImgSingle => {
+        console.log(myImgSingle.intersectionRatio);
+
+        if(myImgSingle.intersectionRatio > 0){
+            loadImage(myImgSingle.target);
+        }
+    })
+}
+
+function loadImage(image){
+    image.src = image.getAttribute('data');
+}
+
+const observer = new IntersectionObserver(handleImg, options);
+
+images.forEach ( img => {
+    observer.observe(img);
+})
