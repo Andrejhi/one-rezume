4d0920e2bfd1a66f0f18f16edf569ec29907deaf
andru <room861@yandex.ru>
2020-01-06 19:19:27 +0300

    Add files via upload

--- jobs/blog-Darja/script/script.js
+++ jobs/blog-Darja/script/script.js
@@ -1,9 +1,14 @@
+// $('.burger_menu').on('click', () => {
+//     $(this).toggleClass('menu_burger_active');
+// });
+
 const searchForm = document.querySelector('.search_form'),
     searchHeader = document.querySelector('.search_header'),
     searchIcon = document.querySelector('.search_icon'),
     closeSearch = document.querySelector('.close_search'),
     openSearch = document.querySelector('.open-search'),
-    searchFormSmart = document.querySelector('.search_form_smart');
+    searchFormSmart = document.querySelector('.search_form_smart'),
+    burgerMenu = document.querySelector('.burger_menu');
 
 searchIcon.addEventListener('click', () => {
     searchForm.classList.add('open_search');
@@ -17,13 +22,13 @@ closeSearch.addEventListener('click', () => {
     closeSearch.style.display = 'none';
 });
 searchIcon.addEventListener('click', () => {
-    searchForm.style.top = '81px';
+    searchForm.style.top = '100px';
     searchFormSmart.style.top = '57px';
     searchIcon.style.display = 'none';
     closeSearch.style.display = 'flex';
 });
 
-const images = document.querySelectorAll('img');
+const imgPost = document.querySelectorAll('.img_post');
 
 const options = {
     root : null,
@@ -41,12 +46,16 @@ function handleImg( myImg, observer){
     })
 }
 
-function loadImage(image){
-    image.src = image.getAttribute('data');
+function loadImage(imgPost){
+    imgPost.src = imgPost.getAttribute('data');
 }
 
 const observer = new IntersectionObserver(handleImg, options);
 
-images.forEach ( img => {
-    observer.observe(img);
+imgPost.forEach (imgPost => {
+    observer.observe(imgPost);
 })
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.add('menu_burger_active');
+})
+
